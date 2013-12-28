033da7b2411c441c062c07e7af6a2ac897af1e8f
jantonio8403 <antonio_108403@hotmail.com>
2013-12-27 18:43:10 -0600

    inicio

---
+++ js/init.js
@@ -0,0 +1,88 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function() {
+
+
+/*----------------------------------------------------*/
+/*	Flexslider
+/*----------------------------------------------------*/
+   $('#intro-slider').flexslider({
+      animation: 'fade',
+      controlNav: false,
+   });
+
+/*----------------------------------------------------*/
+/*	gmaps
+------------------------------------------------------*/
+
+   var map;
+
+   // main directions
+   map = new GMaps({
+      el: '#map', lat: 17.999856, lng: -92.944958, zoom: 15, zoomControl : true,
+      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
+   });
+
+   // add address markers
+   map.addMarker({ lat: 17.999856, lng: -92.943958, title: 'Centro Administrativo de Gobierno',
+   infoWindow: { content: '<p>Secretaria de salud de Tabasco</p>' } });
+
+/*----------------------------------------------------*/
+/*	contact form
+------------------------------------------------------*/
+
+   $('form#contactForm button.submit').click(function() {
+
+      $('#image-loader').fadeIn();
+
+      var contactName = $('#contactForm #contactName').val();
+      var contactEmail = $('#contactForm #contactEmail').val();
+      var contactSubject = $('#contactForm #contactSubject').val();
+      var contactMessage = $('#contactForm #contactMessage').val();
+
+      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+      $.ajax({
+
+	      type: "POST",
+	      url: "inc/sendEmail.php",
+	      data: data,
+	      success: function(msg) {
+
+            // Message was sent
+            if (msg == 'OK') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();   
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
+
+      });
+
+      return false;
+
+   });
+
+
+});
+
+
+
+
+
+
+
+
